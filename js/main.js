f9413478c710889f3bcaf04274a8c45361e84bfd
Md Rinku <79092161+Md-Ringku@users.noreply.github.com>
2021-06-13 13:13:08 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  var typed = new Typed('.type', {
+    strings: ['Company', 'Protection'],
+    typeSpeed: 90,
+    backSpeed: 90,
+    loop: true
+});
+
+new WOW().init();
+});
